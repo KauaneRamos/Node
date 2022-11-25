609624918cd9c26512bd4c355196f19bf6512b5e
KauaneRamos <kauanef.faria@gmail.com>
2022-11-25 09:41:22 -0300

    versão 01

--- aula1.js
+++ aula1.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req,res){
-    res.write("BRASIL 02 X 1 SERVIA");
+    res.write("BRASIL 02 X 0 SERBIA");
       res.end("___bom dia ")
 }).listen(3000)
