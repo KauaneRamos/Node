0f0f59bab957df9c4521a2b2cd2137127b1c0450
KauaneRamos <kauanef.faria@gmail.com>
2022-11-25 10:39:11 -0300

    add dia da semana

--- aula1.js
+++ aula1.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var dia = require('./biblioteca');
 http.createServer(function (req,res){
     res.writeHead(200, {'content-Type': 'text/html'});
     res.write("<h2> BRASIL 02 X 0 SERBIA</h2>");  
-    res.write(Date().substring(16,24)+'<br');
+    res.write('<p style=color:pink>'+Date().substring(16,24)+'<br>');
+    res.write(dia.diaDaSemana()+'<br>')
     res.end("___GANHAMOS ")
 }).listen(3000)
