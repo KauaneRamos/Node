9166330b56fe7b164546c2f3dd6fa50f17aaab04
KauaneRamos <kauanef.faria@gmail.com>
2022-11-25 10:11:34 -0300

    mudei placar e data

--- aula1.js
+++ aula1.js
@@ -1,5 +1,7 @@
 var http = require('http');
 http.createServer(function (req,res){
-    res.write("BRASIL 02 X 0 SERBIA");
-      res.end("___bom dia ")
+    res.writeHead(200, {'content-Type': 'text/html'});
+    res.write("<h2> BRASIL 02 X 0 SERBIA</h2>");  
+    res.write(Date().substring(16,24)+'<br');
+    res.end("___GANHAMOS ")
 }).listen(3000)
