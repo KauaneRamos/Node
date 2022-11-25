19f4e76e8ee642b5d482f891b1ed0621a20dd1b4
KauaneRamos <kauanef.faria@gmail.com>
2022-11-25 10:41:53 -0300

    add dia da semana/mudei placar

--- aula1.js
+++ aula1.js
@@ -2,7 +2,7 @@ var http = require('http');
 var dia = require('./biblioteca');
 http.createServer(function (req,res){
     res.writeHead(200, {'content-Type': 'text/html'});
-    res.write("<h2> BRASIL 02 X 0 SERBIA</h2>");  
+    res.write("<h2> BRASIL 02 X 1 SUIÇA</h2>");  
     res.write('<p style=color:pink>'+Date().substring(16,24)+'<br>');
     res.write(dia.diaDaSemana()+'<br>')
     res.end("___GANHAMOS ")
